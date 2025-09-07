34bbb428561dec0a06ce342f277dd6df38bc7db4
LucasJZ12 <lucas.jieyu.zhou@gmail.com>
2025-09-06 22:57:06 -0700

    Ok I wrote stuff

--- DrawingPanel.java
+++ DrawingPanel.java
@@ -12,12 +12,12 @@ class DrawingPanel extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         // Draw a filled red rectangle
-        g2d.setColor(Color.RED);
+        g2d.setColor(Color.GREEN);
         g2d.fillRect(50, 50, 100, 80);
 
         // Draw blue text
         g2d.setColor(Color.BLUE);
         g2d.setFont(new Font("Arial", Font.BOLD, 24));
-        g2d.drawString("Hello Graphics!", 50, 200);
+        g2d.drawString("AHHHHHHHHHHH", 350, 350);
     }
 }
--- What.java
+++ What.java
@@ -1,3 +1,13 @@
+import javax.swing.JFrame;
 public class What{
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test 1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 700);
+
+        DrawingPanel panel = new DrawingPanel();
+        frame.add(panel);
+
+        frame.setVisible(true);
+    }
 }
